a55dcb57b719651b99d8ff3241669e9ab5439a13
FrancescoRosi01 <139993451+FrancescoRosi01@users.noreply.github.com>
2025-10-02 20:16:51 +0200

    aggiunto prima versione modello

--- django_project/shortly/models.py
+++ django_project/shortly/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ShortenedUrl(models.Model):
+    base_url = models.URLField()
+    short_url = models.URLField(db_index=True)
+    alias = models.TextField(max_length=50)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
